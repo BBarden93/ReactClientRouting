5929af1a7f2d7959ff37777a55265f782d17ee79
Brittany Barden <bbarden93@yahoo.com>
2018-03-30 11:41:27 -0700

    about view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Home from './views/Home.js'
+import About from './views/About.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Home />
+        <About />
       </div>
     );
   }
