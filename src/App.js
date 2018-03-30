dc3e45e066d480ec4014316ec2fd3d6813bba545
Brittany Barden <bbarden93@yahoo.com>
2018-03-30 12:44:25 -0700

    redirects

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Home from './views/Home.js'
 import About from './views/About.js'
 import Contact from './views/Contact.js'
-import {Route, Link} from 'react-router-dom'
+import {Route, Link, Redirect} from 'react-router-dom'
 
 class App extends Component {
   render() {
@@ -13,12 +13,19 @@ class App extends Component {
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Link to="/contact">Contact</Link>
+            <Link to="/vip">VIP</Link>
         </div>
         
             <Route exact={true} path='/' component={Home} />
             <Route path='/about' component={About}/>
             <Route path='/contact' component={Contact} />
             
+            <Route path="/vip" render={() => {
+              const loggedIn = false
+              if(!loggedIn) return <Redirect to='/' />
+              return <h1>You are a VIP. Welcome</h1>
+            }} />
+
       </div>
     );
   }
